c918959951af378f57cf546493cc2ef112e497e7
Kevin <bourkekev@gmail.com>
2020-05-02 11:10:23 +0100

    Finish single marker with info window

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,10 +1,10 @@
 function initMap() {
     //map options
     let options = {
-        zoom: 10,
+        zoom: 12,
         center: {
-            lat: 53.3498,
-            lng: -6.2603
+            lat: 53.4256,
+            lng: -6.1016
         }
     }
     let map = new google.maps.Map(document.getElementById("map"), options);
@@ -13,10 +13,20 @@ function initMap() {
     let iconBase = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/';
     
     var marker = new google.maps.Marker({
-        position:  { lat: 53.4256, lng: -6.1316 },
-        map: map
+        position:  { lat: 53.4246, lng: -6.1210 },
+        map: map,
+        icon: iconBase + "beachflag.png"
     });
 
+    //info window
+    let infoWindow = new google.maps.InfoWindow({
+        content: '<h2>Velvet Strand</h2><p>Portmarnock</p><h3>Hi Lynn!</h3>'
+    });
+
+    // need to add listener to listen for that info window
+    marker.addListener('click', function(){
+        infoWindow.open(map, marker)
+    });
 
     // CI Code for map markers and clusters below
     //let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -36,6 +46,6 @@ function initMap() {
     // });
 
     // Add a marker clusterer to manage the markers.
-    var markerCluster = new MarkerClusterer(map, markers,
-        {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+    // var markerCluster = new MarkerClusterer(map, markers,
+    //     {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 }
