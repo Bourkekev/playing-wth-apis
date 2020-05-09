31a963e5e05a132d45eb97c570825210878f58aa
Kevin Bourke <kevinbourke@Kevins-iMac.local>
2020-05-09 09:20:49 +0100

    Added polylines and polygons

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,5 +1,5 @@
 //Need to delare these first for Geolocating as a function hapeens outside of initMap
-let map, infoWindow;
+let map, infoWindow, flightPath;
 
 function initMap() {
     //map options
@@ -10,7 +10,7 @@ function initMap() {
             lng: -6.1016
         }
     }
-    let map = new google.maps.Map(document.getElementById("map"), options);
+    map = new google.maps.Map(document.getElementById("map"), options);
     infoWindow = new google.maps.InfoWindow;
 
     // Listen for click event on map. 3 paramters the map to listen on (variable map), the event to listen for and a function and we pass the event into the function.
@@ -163,7 +163,32 @@ function initMap() {
     // Add a marker clusterer to manage the markers.
     // var markerCluster = new MarkerClusterer(map, markers,
     //     {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+
+    // TEST OUT POLYLINE
+    let myTrip = [
+        { lat: 53.4256, lng: -6.1316 },
+        { lat: 53.4509, lng: -6.1501 },
+        { lat: 53.3786, lng: -6.0570 }
+    ];
+    flightPath = new google.maps.Polyline({
+        path: myTrip,
+        strokeColor:"#0000FF",
+        strokeOpacity:0.8,
+        strokeWeight:1
+    });
+    // To use a POLYGON just use .Polygon in above example
+    // call function to add line
+    addLine();
 }
+function addLine() {
+    // set the map to put polyline on
+    flightPath.setMap(map);
+  }
+
+function removeLine() {
+    flightPath.setMap(null);
+}
+
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
